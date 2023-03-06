440819e5aaf4fc5ca1dc9deac5a696b346f4654d
Illya <drozd.vol@gmail.com>
2023-03-06 22:40:38 +0200

    add buttons menu

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const botToken = '6134933127:AAHG5mHmFJu2qtWEAKMmxBklq8JLnWhkFdw';
 
 const bot = new TelegramBot(botToken, {polling: true});
 
-bot.on('message', msg => {
+bot.on('message', async msg => {
     console.log(msg);
     let chatId = msg.chat.id;
 
     if(msg.text === '/start')
-
-    bot.sendMessage(chatId, 'Пліветь)');
+        await bot.sendMessage(chatId, 'Пліветь', { reply_markup:{ "keyboard": [["/start"],["Хто підор?", "Скільки нас?"]]}});
     // bot.sendsticker(chatId, 'https://tlgrm.eu/_/stickers/9e9/6dc/9e96dc9a-90ed-3994-9c2f-d2a269f548d4/9.webp');
+    if(msg.text === 'Хто підор?')
+        await bot.sendMessage(chatId, 'Владік');
 })
